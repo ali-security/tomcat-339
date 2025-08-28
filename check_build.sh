fca57dfe902d5cb76211c7e0a2c05eeb76a9bc35
Uriel Cohen <uriel.c@sealsecurity.io>
2025-08-28 11:44:30 +0300

    added test for changes

---
+++ check_build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+rm -r build-logs
+gh run download $(gh run list --limit 1 --json databaseId --jq '.[0].databaseId')
+
+origin_failures=$(grep -L "Failures: 0" default-logs/build/logs/* | xargs -n1 basename | sort)
+origin_errors=$(grep -L "Errors: 0" default-logs/build/logs/* | xargs -n1 basename | sort)
+
+failures=$(grep -L "Failures: 0" build-logs/build/logs/* | xargs -n1 basename | sort)
+errors=$(grep -L "Errors: 0" build-logs/build/logs/* | xargs -n1 basename | sort)
+
+# Compare original failures to new failures and print new ones
+echo "Original Failures:"
+echo $origin_failures       
+echo "New Failures:"
+echo $failures
+echo "--------------------------------"
+echo "Original Errors:"
+echo $origin_errors
+echo "New Errors:"
+echo $errors
+echo "--------------------------------"
+echo "Changed Failures:"
+comm -3 <(echo "$origin_failures_list") <(echo "$failures_list")
+echo "--------------------------------"
+echo "Changed Errors:"
+comm -3 <(echo "$origin_errors_list") <(echo "$errors_list")
